d90a5ae639e013e196229d594d237634dcab2cd4
admin <isalechat@yahoo.com>
2018-01-18 19:01:28 +0100

    on s'attaque au nom des villes

--- route_de_la_mairie.rb
+++ route_de_la_mairie.rb
@@ -4,7 +4,15 @@ require 'open-uri'
 # marc
 
 def mairies_du_95
-  ville = {nom:'',email:''}
+  town_hash = {name:'',url:'',email:''}
+
+  def get_town_name
+    townhalls = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    names = townhalls.css('a')
+    names.each do |name|
+    # town_hash[:name] = name
+    end
+  end
 
   def get_the_email_of_a_townhal_from_its_webpage
     vaureal = "http://annuaire-des-mairies.com/95/vaureal.html"
@@ -26,12 +34,15 @@ def mairies_du_95
       new_link[0] = ''
       new_link[0] = ''
       puts ("http://annuaire-des-mairies.com/"+new_link)
+      return name:name
     end
 
   end
 
-  get_all_the_urls_of_val_doise_townhalls()
-  get_the_email_of_a_townhal_from_its_webpage()
-  
+  get_town_name()
+  # get_all_the_urls_of_val_doise_townhalls()
+  # get_the_email_of_a_townhal_from_its_webpage()
+  puts town
+
 end
 mairies_du_95()
